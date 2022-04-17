4ed2e6f78ea6ebe9733270b72e7e1124bddb1756
chris0016 <cper3745@lehmanhs.com>
2022-04-17 12:36:48 -0400

    Update README

--- Configs.java
+++ Configs.java
@@ -3,6 +3,6 @@ public class Configs {
 	protected static String dbhost = "localhost";
 	protected static String dbport = "3306";
 	protected static String username = "root";
-	protected static String password = "361.77Mlqs@@";
-	protected static String dbname = "university_30603a";
+	protected static String password = "";
+	protected static String dbname = "";
 }
